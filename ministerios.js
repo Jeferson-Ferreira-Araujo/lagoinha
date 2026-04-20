d4dc14108fe0bd3d1bc7da5423794686322f903a
Jeferson Ferreira de Araújo <jeferson.araujo@lmcorp.local>
2026-04-20 17:30:24 -0300

    Primeiro commit

---
+++ ministerios.js
@@ -0,0 +1,146 @@
+const ministerios = [
+  {
+    nome: "Legacy",
+    coordenacao: "A definir",
+    telefone: "",
+    imagem: "",
+    descricao: "",
+    galeria: [],
+    video: ""
+  },
+  {
+    nome: "Louvor",
+    coordenacao: "A definir",
+    telefone: "",
+    imagem: "",
+    descricao: "",
+    galeria: [],
+    video: ""
+  },
+  {
+    nome: "Kids",
+    coordenacao: "A definir",
+    telefone: "",
+    imagem: "",
+    descricao: "",
+    galeria: [],
+    video: ""
+  },
+  {
+    nome: "Link",
+    coordenacao: "A definir",
+    telefone: "",
+    imagem: "",
+    descricao: "",
+    galeria: [],
+    video: ""
+  },
+  {
+    nome: "Interseção",
+    coordenacao: "A definir",
+    telefone: "",
+    imagem: "",
+    descricao: "",
+    galeria: [],
+    video: ""
+  },
+  {
+    nome: "Diaconato",
+    coordenacao: "A definir",
+    telefone: "",
+    imagem: "",
+    descricao: "",
+    galeria: [],
+    video: ""
+  },
+  {
+    nome: "Wecare",
+    coordenacao: "A definir",
+    telefone: "",
+    imagem: "",
+    descricao: "",
+    galeria: [],
+    video: ""
+  },
+  {
+    nome: "Mídia",
+    coordenacao: "A definir",
+    telefone: "",
+    imagem: "",
+    descricao: "",
+    galeria: [],
+    video: ""
+  },
+  {
+    nome: "Conect",
+    coordenacao: "A definir",
+    telefone: "",
+    imagem: "",
+    descricao: "",
+    galeria: [],
+    video: ""
+  },
+  {
+    nome: "Hero",
+    coordenacao: "A definir",
+    telefone: "",
+    imagem: "",
+    descricao: "",
+    galeria: [],
+    video: ""
+  },
+  {
+    nome: "Shine",
+    coordenacao: "A definir",
+    telefone: "",
+    imagem: "",
+    descricao: "",
+    galeria: [],
+    video: ""
+  },
+  {
+    nome: "Crie",
+    coordenacao: "A definir",
+    telefone: "",
+    imagem: "",
+    descricao: "",
+    galeria: [],
+    video: ""
+  },
+  {
+    nome: "Gerações",
+    coordenacao: "A definir",
+    telefone: "",
+    imagem: "",
+    descricao: "",
+    galeria: [],
+    video: ""
+  },
+  {
+    nome: "Dança",
+    coordenacao: "A definir",
+    telefone: "",
+    imagem: "",
+    descricao: "",
+    galeria: [],
+    video: ""
+  },
+  {
+    nome: "GC",
+    coordenacao: "A definir",
+    telefone: "",
+    imagem: "",
+    descricao: "",
+    galeria: [],
+    video: ""
+  },
+  {
+    nome: "Mãos Ungidas",
+    coordenacao: "A definir",
+    telefone: "",
+    imagem: "",
+    descricao: "",
+    galeria: [],
+    video: ""
+  }
+];
